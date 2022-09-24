fe7134a74af1ced49b307978d8bcfdaaaae3bbe6
rvzdnk <rafal93zdunek@gmail.com>
2022-09-24 18:38:28 +0200

    final

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -4,7 +4,7 @@ import { ImageWrapper } from './MovieDetails.styled';
 export const MovieDetails = ({ movieById }) => {
     const { poster_path, release_date, title, vote_average, overview, genres } = movieById;
 
-    const releaseYear = release_date.slice(0, 4);
+    const releaseYear = new Date(release_date).getFullYear();
     const userScore = `${(vote_average * 10).toFixed(0)}%`;
     const genreName = genres?.map(genre => genre.name).join(', ');
 
@@ -17,7 +17,7 @@ export const MovieDetails = ({ movieById }) => {
                 />
             </div>
             <div>
-                <h2>{`${title} (${releaseYear}`}</h2>
+                <h2>{`${title} (${releaseYear})`}</h2>
                 <p>{`User score: ${userScore}`}</p>
                 <h3>Overview</h3>
                 <p>{overview}</p>
